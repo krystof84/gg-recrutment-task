3c290feb42af767142b5617137b2ac7500ba15fc
Krzysztof <krisczajka@gmail.com>
2018-08-14 12:02:52 +0200

    Add reset stylesheet. Add google font

--- functions.php
+++ functions.php
@@ -8,12 +8,15 @@ function theme_scripts() {
     // Theme css file
     wp_enqueue_style('bb-style', get_template_directory_uri() . '/style.css', array(), '0.1');   
 
+    // Add google font
+    wp_enqueue_style('bb-font-fisrt', 'https://fonts.googleapis.com/css?family=Open+Sans', array(), null);
+
     // Main css file
     wp_enqueue_style('bb-main', get_template_directory_uri() . '/src/css/main.css', array(), '0.1');
 
     // Live reload - only dev enviroment
     if($_SERVER['SERVER_NAME'] == 'globegroup') {
-        wp_enqueue_script('livereload', 'http://kalypso:35729/livereload.js?snipver=1', null, false, true);
+        wp_enqueue_script('livereload', 'http://globegroup:35729/livereload.js?snipver=1', null, false, true);
     }
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts');
